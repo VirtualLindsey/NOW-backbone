28f7cadfae918614cc489d830483601f5d017caf
VirtualLindsey <tapircode@gmail.com>
2017-11-27 15:23:49 -0800

    adding songs

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // Vendor Modules
 import $ from 'jquery';
 import _ from 'underscore';
+import Song from './models/song';
 
 // CSS
 import './css/foundation.css';
@@ -8,6 +9,31 @@ import './css/style.css';
 
 console.log('it loaded!');
 
+const songData = [
+    {
+        title: "Drop It Like It's Hot",
+        year: 2004,
+        artist: "Snoop Dog"
+    },
+    {
+        title: "Thriller",
+        year: 1982,
+        artist: "Michael Jackson"
+    },
+    {
+        title: "Blue Monday",
+        year: 1983,
+        artist: "New Order"
+    }
+  ];
+
 $(document).ready( () => {
-  
+  const song = new Song({
+    title: 'hi',
+    artist: 'hi bye',
+    year: 2010
+  });
+
+  console.log('attirbutes are');
+  console.log(song.attributes);
 });
